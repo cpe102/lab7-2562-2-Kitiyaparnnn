066a884127590b6184a2bdb6fea7e96959bdfcd4
lab314-14@labeng.local <lab314-14@labeng.local>
2020-01-21 15:21:12 +0700

    final

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,45 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string status;
+    int age,h,money;
+    cout<<"How old are you :) ?? : ";
+    cin>>age;
+
+    if(age<=20)
+    {
+        cout<<"Your hight(centimetre): ";
+        cin>>h;
+        if(h<=160){
+            status = "UNFRIEND";
+        }
+        else if(h<=175){
+            status = "FRIEND";
+        }
+        cout<<"How much money have you had?? (million) : ";
+        cin>>money;
+        if(money>=69){
+            status = "MARRIED";
+        }
+        else{
+            status = "ONE-NIGHT-STAND";
+        }
+    }
+    else if(age<30){
+        cout<<"How much money have you had?? (million) : ";
+        cin>>money;
+        if(money>10){
+            status = "BEST FRIEND";
+        }
+        else{
+            status = "UNFRIEND";
+        }
+    }
+    else status = "UNFRIEND";
+
+    cout<<status;
+}
